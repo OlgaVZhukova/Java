6f62c74878418a39afa1c5f5545015644851d502
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-02-11 14:05:47 +0300

    HW1 Task 1 updated

--- Homeworks/HW1/task1.java
+++ Homeworks/HW1/task1.java
@@ -2,6 +2,22 @@
 
 package Homeworks.HW1;
 
+import java.util.Scanner;
 public class task1 {
-    
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.printf("Введите число: ");
+        int n = Scanner.nextInt();
+        Scanner.close();
+        System.out.printf("Треугольное число = %s", Triangle(n));
+    }
+
+
+    static int Triangle(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return Triangle(num - 1) + num;
+    }
 }
+
